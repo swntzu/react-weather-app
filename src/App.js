f13faeda2bf219064eb0e0f9fc1baab261bd0b6a
Kar Meng Lee <karmenglee@hotmail.com>
2022-07-20 00:11:05 +0200

    Forecast preparation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CurrentWeather from "./components/current-weather/current-weather";
 import Search from "./components/search/search";
 import { WEATHER_API_URL } from "./api";
 import { useState } from "react";
+import Forecast from "./components/forecast/forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -37,6 +38,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,12 @@
+import { Accordion } from "react-accessible-accordion";
+
+const Forecast = ({ data }) => {
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded></Accordion>
+    </>
+  );
+};
+
+export default Forecast;
